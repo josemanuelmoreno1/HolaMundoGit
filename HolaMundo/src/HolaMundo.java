ac09957feb5f12e2ed39915f1c1c831b313d6c44
debian <debian@debian>
2018-10-22 01:03:56 +0200

    2 Cambio Eclipse

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("1 cambio - Consola");
 		System.out.println("1 cambio - Eclipse");
+		System.out.println("2 cambio - Eclipse");
 	}
 
 }
